5c36ab47e75a61fe2624b58bc33f1b8346a941ff
Guimpontes <guimotapontes624@gmail.com>
2022-09-14 14:07:14 -0300

    quinto-dia

--- Quinto-desafio/script.js
+++ Quinto-desafio/script.js
@@ -1,168 +1,61 @@
+let adicionarItem = 'sim'
 
-// Campo de adicionar um item na lista.
-let inputAdicionar = document.querySelector('#inputAdicionar');
-let btnAdicionar = document.querySelector('#btn-adicionar');
-let listaDeItens = document.querySelector('.lista');
+let frutas = [];
+let laticinios = [];
+let congelados = [];
+let doces = [];
+let proteinas = [];
+let graos = [];
 
-// Campo de adicionar categorias.
-let inputCategoria = document.querySelector('#inputCategoria');
-let btnAdicionarCategoria = document.querySelector('#btn-adicionar-categoria');
-let categoria = document.querySelector('.categorias');
+while (adicionarItem == 'sim') {
 
-// Adicionar categoria com a tecla enter
-inputCategoria.addEventListener('keypress', (e) => {
+    adicionarItem = prompt('Você deseja adicionar algum item na lista? sim ou não').toLowerCase();
 
-    //Tecla enter
-    if (e.keyCode == 13) {
+    while (adicionarItem != 'sim' && adicionarItem != 'não') {
+        alert('Essa resposta é inválida. Tente novamente');
 
-        //Validando se o input de adicionar uma categoria está vazio.
-        if (inputCategoria.value != "") {
-            let div = document.createElement('div');
-
-            let input = document.createElement('input');
-            input.setAttribute('type', 'radio');
-            input.setAttribute('name', 'categoria')
-            input.setAttribute('onclick', 'filtrarItem(this)')
-            input.id = inputCategoria.value;
-
-            let label = document.createElement('label');
-            label.setAttribute('for', inputCategoria.value);
-            label.setAttribute('name', 'categoria')
-            label.textContent = inputCategoria.value;
-
-            div.appendChild(input);
-            div.appendChild(label);
-
-            categoria.appendChild(div);
-
-            inputCategoria.value = '';
-        } else {
-            alert('Você deve preencher o campo com uma categoria.')
-        }
+        adicionarItem = prompt('Você deseja adicionar algum item na lista? sim ou não').toLowerCase();
     }
-})
 
-// Adicionar uma categoria
-btnAdicionarCategoria.addEventListener('click', (e) => {
+    if (adicionarItem === 'não') {
 
-    // Validando se o input de adicionar uma categoria está vazio.
-    if (inputCategoria.value != "") {
-        let div = document.createElement('div');
+        alert(`
+               Frutas: 
+                  ${frutas}
 
-        let input = document.createElement('input');
-        input.setAttribute('type', 'radio');
-        input.setAttribute('name', 'categoria')
-        input.setAttribute('onclick', 'filtrarItem(this)')
-        input.id = inputCategoria.value;
+               laticínios: 
+                  ${laticinios} 
 
-        let label = document.createElement('label');
-        label.setAttribute('for', inputCategoria.value);
-        label.setAttribute('name', 'categoria')
-        label.textContent = inputCategoria.value;
+               congelados:
+                  ${congelados}
 
-        div.appendChild(input);
-        div.appendChild(label);
+               doces: 
+                  ${doces}
 
-        categoria.appendChild(div);
+               Proteínas:
+                  ${proteinas}
 
-        inputCategoria.value = '';
+               grãos: 
+                  ${graos}`)
+
+        break;
+    }
+
+    let item = prompt('Qual item você gostaria de adicionar?');
+    let categoria = prompt('Qual categoria esse item se encaixa? frutas, laticínios, congelados, doces, proteínas ou grãos ').toLowerCase();
+
+    if (categoria == 'frutas') {
+        frutas.push(item)
+    } else if (categoria == 'laticínios') {
+        laticinios.push(item)
+    } else if (categoria == 'congelados') {
+        doces.push(item);
+    } else if (categoria == 'proteínas') {
+        proteinas.push(item);
+    } else if (categoria == 'grãos') {
+        graos.push(item);
     } else {
-        alert('Você deve preencher o campo com uma categoria.')
+        alert('Categoria inválida')
     }
-});
-
-// Lista de item
-let item = [];
-
-// Função para criar o item da lista.
-function criarItem(item, categoria) {
-    return { item, categoria }
 }
 
-// Função para adicionar o item na lista pressionando a tecla enter.
-inputAdicionar.addEventListener('keypress', (e) => {
-
-    //Tecla enter
-    if (e.keyCode == 13) {
-
-        //Validando se o usuário criou uma categoria.
-        if (categoria.children.length > 0) {
-
-            //Validando se o usuário selecionou uma categoria.
-            if (categorias.length > 0) {
-
-                //Validando se o campo de adicionar um item na lista está vazio.
-                if (inputAdicionar.value != "") {
-                    item.push(criarItem(inputAdicionar.value, categorias[0]))
-                    inputAdicionar.value = '';
-
-                    filtrar();
-                } else {
-                    alert("Você deve preencher o campo com um item.")
-                }
-
-            } else {
-                alert('Selecione uma categoria.')
-            }
-        } else {
-            alert("Você não tem nenhuma categoria. Crie uma e selecione ela antes de adicionar um item na lista")
-        }
-    }
-})
-
-// Função para adicinar o item na lista clicando o botão adicionar.
-btnAdicionar.addEventListener('click', (e) => {
-
-    //Validando se o usuário criou uma categoria.
-    if (categoria.children.length > 0) {
-
-        //Validando se o usuário selecionou uma categoria.
-        if (categorias.length > 0) {
-
-            //Validando se o campo de adicionar um item na lista está vazio.
-            if (inputAdicionar.value != "") {
-                item.push(criarItem(inputAdicionar.value, categorias[0]))
-                inputAdicionar.value = '';
-
-                filtrar();
-            } else {
-                alert("Você deve preencher o campo com um item.")
-            }
-
-        } else {
-            alert('Selecione uma categoria.')
-        }
-    } else {
-        alert("Você não tem nenhuma categoria. Crie uma e selecione ela antes de adicionar um item na lista")
-    }
-
-})
-
-function filtrar() {
-
-    //Filtrando os items da lista de acordo com o a categoria selecionada.
-    let filterList = item.filter((item) => {
-        return item.categoria == categorias[0];
-    })
-
-    listaDeItens.innerHTML = filterList.map((item) => `<li>${item.item}</li>`).join(' ');
-}
-
-
-// Lista de categorias
-let categorias = [];
-
-// Função para filtrar um item da lista.
-function filtrarItem(elemento) {
-
-    categorias = [];
-
-    let includes = categorias.includes(elemento.id);
-
-
-    if (!includes) {
-        categorias.push(elemento.id)
-    }
-
-    filtrar();
-}
